eba7e8d7344268d845ffea5e0361dbdbe8229e0a
AhmedI99 <ahmedibrahim5182@gmail.com>
2024-10-10 23:06:16 +0300

    fixing loading images, fixing paths for local development

--- frontend/src/app/app.apiRoutes.ts
+++ frontend/src/app/app.apiRoutes.ts
@@ -16,7 +16,13 @@ export const API_IMAGE_PATH
   = 'http://localhost:8888/.netlify/functions/serveImage?imageName=';
 
 export const API_PRODUCTS_FILE_LOCATION
-  = '';
+  = './backend/data/products.json';
+
+export const API_USER_CART_LOCATION
+  = './backend/data/user-cart.json';
+
+export const API_IMAGES_LOCATION
+  = './backend/images';
 */
 
 
@@ -27,6 +33,7 @@ export const API_PRODUCTS_FILE_LOCATION
 
 
 // For netlify production
+
 export const API_GET_PRODUCTS_PATH
   = '/.netlify/functions/getProducts';
 
@@ -40,7 +47,7 @@ export const API_DELETE_USER_PRODUCT_PATH
   = '/.netlify/functions/deleteUserCart';
 
 export const API_IMAGE_PATH
-  = '/.netlify/functions/serveImage?imageName=';
+  = 'https://shopco-fs-webmasters.netlify.app/.netlify/functions/serveImage?imageName=';
 
 export const API_PRODUCTS_FILE_LOCATION
   = '/var/task/backend/data/products.json';
@@ -49,6 +56,8 @@ export const API_USER_CART_LOCATION
   = '/var/task/backend/data/user-cart.json';
 export const API_IMAGES_LOCATION
   = '/var/task/backend/images';
+
+
 
 
 // for node.js ( launching frontend's and backend's package.json
