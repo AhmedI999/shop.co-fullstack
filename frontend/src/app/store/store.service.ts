85908eb9a4c16204ee130e7dd13558e186a9af45
AhmedI99 <ahmedibrahim5182@gmail.com>
2024-10-16 22:25:35 +0300

    debug addProductTO

--- frontend/src/app/store/store.service.ts
+++ frontend/src/app/store/store.service.ts
@@ -40,7 +40,7 @@ export class StoreService {
   loadUserCart(isNetlify: boolean = false) {
     if (isNetlify) {
       const cartFromLocalStorage = localStorage.getItem('userCart');
-      if (!cartFromLocalStorage) localStorage.setItem('userCart', JSON.stringify());
+      // if (!cartFromLocalStorage) localStorage.setItem('userCart', JSON.stringify(Array.from(new Product())));
       const parsedCart = JSON.parse(cartFromLocalStorage!);
       this.userCart.set(parsedCart);
       this.calculateCartTotal();
